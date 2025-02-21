69a219710b28bad74bd7020c90e1e0e0ad335eba
BrunoCavalli <87094109+BrunoCavalli@users.noreply.github.com>
2025-02-21 20:35:09 -0300

    add novas funçÃões

--- main.cpp
+++ main.cpp
@@ -12,29 +12,49 @@ int subtracao(int x, int y) {
     return x - y;
 }
 
-int main() {
-    int a;
-    int b;
-    int operacao;
+float divisao(float x, float y) {
+    return x / y;
+}
 
-    cout << "Escolha uma operacao (+ para soma, - para subtracao): ";
+float raiz(int y) {
+    return sqrt(y);
+}
+
+int main() {
+    float a;
+    float b;
+    char operacao;
+
+    cout << "Escolha uma operacao (+ para soma, - para subtracao, / para divisao, R para raiz): ";
     std::cin >> operacao;
 
-    std:cout << "digite um valor:";
-    std::cin >> a;
 
-    cout << "digite outro valor:";
-    std::cin >> b;
+    float resultado;
+    if (operacao == '+' || operacao == '-' || operacao == '/') {
+        std:cout << "digite um valor:";
+        std::cin >> a;
 
-    int resultado;
-    if (operacao == '+') {
-        resultado = soma(a, b);
-    } else if (operacao == '-') {
-        resultado = subtracao(a, b);
-    } else {
+        cout << "digite outro valor:";
+        std::cin >> b;
+
+        if (operacao == '+') {
+            resultado = soma(a, b);
+        } else if (operacao == '-') {
+            resultado = subtracao(a, b);
+        } else if (operacao == '/') {
+            resultado = divisao(a, b);
+    }
+    } else if (operacao == 'R') {
+        cout << "digite um valor:";
+        std::cin >> a;
+        resultado = raiz(a);
+    }
+    else {
         cout << "Erro, tente novamente" << endl;
         return 1;
     }
+
+
 
     cout << resultado << endl;
 
